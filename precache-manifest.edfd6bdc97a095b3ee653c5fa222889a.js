66d06c5e616201cd72d8f4daba8f6749636fd002
Anna Gerdii <annagerdiy@gmail.com>
2020-01-13 19:38:27 +0200

    Updates

---
+++ precache-manifest.edfd6bdc97a095b3ee653c5fa222889a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c1a35f4b8c2ab46dac283ad0db774c",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "7dc71cb4b05c5ef70f1d",
+    "url": "/react_uber-eats/static/css/main.64fc147b.chunk.css"
+  },
+  {
+    "revision": "dde138b44fd1e42541cd",
+    "url": "/react_uber-eats/static/js/2.b4efc14b.chunk.js"
+  },
+  {
+    "revision": "7dc71cb4b05c5ef70f1d",
+    "url": "/react_uber-eats/static/js/main.d02b14c3.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
